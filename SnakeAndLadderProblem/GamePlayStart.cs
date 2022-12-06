c57d9f146f0d561ebc121422e41ba7ece1935cba
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-06 19:28:14 +0530

    [Mahesh]Add: GamePlayStart

---
+++ SnakeAndLadderProblem/GamePlayStart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    internal class GamePlayStart
+    {
+        public static void Start()
+        {
+            int startGamePosition = 0;
+            Console.WriteLine(" Lets play");
+            Console.WriteLine("You are at player position  "+ startGamePosition);
+            Console.WriteLine("Lets rolls the Die");
+        }
+    }
+}
--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+ namespace SnakeAndLadderProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("****Welcome To Snake and Ladder Problem****");
+                Console.WriteLine("Please Select below options"+"\n"+ "1.GamePlayStart");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                    GamePlayStart.Start();
+                    break;
+                }
+            }
+        }
+    }
+}
